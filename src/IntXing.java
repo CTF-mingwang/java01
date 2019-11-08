7309dddf35c75fb4128fe30a44cb38d7262d1e28
code <2633024983@qq.com>
2019-11-08 15:43:50 +0800

    我重新编辑了一下文件

--- src/IntXing.java
+++ src/IntXing.java
@@ -1,18 +1,31 @@
 public class IntXing {
     public static void main(String[] args) {
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+        zhengXing(); //调用方法
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+        geShiHua(); //调用方法
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
         shuChu(); //调用方法
-        qingDan();
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~");
+        qingDan(); //调用方法
+    }
+    //整数的不同打印方式
+    public static void zhengXing() {
         int value = 2147483647 + 1; //数据异常
-
-        String str = String.format("%.2f%10d",20.3,10);
-        System.out.println(str);
-
         System.out.println(1.0 - 0.9); //是一个无限接近0.1的小数，和正常的数学算法不同
         System.out.println(value); //打印输出
         System.out.println(Integer.MAX_VALUE); //最大值
         System.out.println(Integer.MIN_VALUE); //最小值
     }
 
+    //格式化声明变量 与 输出错误信息
+    public static void geShiHua() {
+        String str = String.format("%.2f%10d",20.3,10); //格式化声明变量
+        System.out.println(str); //打印输出
+        System.err.println("我是bug,我怕谁！！！"); //输出错误信息
+    }
+
+    //电脑配置表
     public static void shuChu() {
         String pingPei = "华硕 ASUS";
         String xingHao = "GTX 1080";
@@ -21,11 +34,12 @@ public class IntXing {
         float kuan = 13.4F;
         float gao = 5.25F;
 
-        System.out.println("\t品牌；" + pingPei + "\n\t型号：" + xingHao + "\n核心频率：" + pinlv + "\n长,宽,高：" + chang + '/' + kuan + '/' +gao + "\n");
+        System.out.println("\t品牌；" + pingPei + "\n\t型号：" + xingHao + "\n核心频率：" + pinlv + "\n长,宽,高：" + chang + '/' + kuan + '/' +gao);
 
 
     }
 
+    //格式化输出吃货清单
     public static void qingDan() {
         System.out.println("某吃货清单\n-----------------------");
         System.out.println("商品名称\t单价\t数量\t备注");
@@ -34,6 +48,6 @@ public class IntXing {
         int count1 = 999;                 //数量
         String remark1 = "憨憨爱吃";      //备注
 
-        System.out.printf("%s\t\t%.2f\t%d\t\t%s\n\n",goodsName1,price1,count1,remark1);
+        System.out.printf("%s\t\t%.2f\t%d\t\t%s",goodsName1,price1,count1,remark1);
     }
 }
